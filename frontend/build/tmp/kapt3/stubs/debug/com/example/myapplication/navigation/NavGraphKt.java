398150b0c124deba3a02b6c1d1143d121a602078
Tejas Anand <tejasanand@Tejass-MacBook-Pro.local>
2025-08-10 11:23:54 +0530

    Add frontend folder

---
+++ frontend/build/tmp/kapt3/stubs/debug/com/example/myapplication/navigation/NavGraphKt.java
@@ -0,0 +1,10 @@
+package com.example.myapplication.navigation;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\n\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0004\u001a\u0015\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u0003H\u0007\u00a2\u0006\u0002\u0010\u0004\u00a8\u0006\u0005"}, d2 = {"NavGraph", "", "navController", "error/NonExistentClass", "(Lerror/NonExistentClass;)V", "app_debug"})
+public final class NavGraphKt {
+    
+    @error.NonExistentClass()
+    public static final void NavGraph(@org.jetbrains.annotations.NotNull()
+    error.NonExistentClass navController) {
+    }
+}
